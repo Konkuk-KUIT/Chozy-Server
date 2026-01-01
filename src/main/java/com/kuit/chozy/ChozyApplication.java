950f2c53500c67937b994cd1f43b29005aaf5ba5
lylylylh <yh81260@naver.com>
2026-01-02 05:07:31 +0900

    [Init] spring boot project setup

---
+++ src/main/java/com/kuit/chozy/ChozyApplication.java
@@ -0,0 +1,13 @@
+package com.kuit.chozy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChozyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChozyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuit/chozy/ChozyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuit.chozy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChozyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
