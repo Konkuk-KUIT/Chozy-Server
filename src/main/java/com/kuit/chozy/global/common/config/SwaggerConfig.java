e03d1c7954024d88dee03866f65d39506e687136
Jinn25 <hyojinn82@gmail.com>
2026-02-12 20:28:35 +0900

    [Refactor] Swagger 주소 변경

--- src/main/java/com/kuit/chozy/global/common/config/SwaggerConfig.java
+++ src/main/java/com/kuit/chozy/global/common/config/SwaggerConfig.java
@@ -2,10 +2,13 @@ package com.kuit.chozy.global.common.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import org.springframework.context.annotation.Bean;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -13,6 +16,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("http://3.37.183.205")
+                ))
                 .components(new Components()
                         .addSecuritySchemes("BearerAuth",
                                 new SecurityScheme()
