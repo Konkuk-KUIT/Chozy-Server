abfb12f506377ca49816edee57fbdd36200e56cb
Jinn25 <hyojinn82@gmail.com>
2026-02-15 05:45:43 +0900

    [Log] Swagger 토큰 로그

--- src/main/java/com/kuit/chozy/global/common/config/SwaggerConfig.java
+++ src/main/java/com/kuit/chozy/global/common/config/SwaggerConfig.java
@@ -5,9 +5,12 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
 public class SwaggerConfig {
 
@@ -31,4 +34,11 @@ public class SwaggerConfig {
                         .description("Chozy Server API Documentation")
                 );
     }
+
+
+    @PostConstruct
+    void loaded() {
+        log.info("[SwaggerConfig] LOADED, BEARER_AUTH={}", BEARER_AUTH);
+    }
+
 }
